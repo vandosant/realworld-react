2b00b28fa353e5d5df86bb04e5f5900b27577534
vandosant <sjskender@gmail.com>
2017-03-18 14:08:56 -0600

    Add graphql to koa

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,43 @@
 const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const koaBody = require('koa-bodyparser');
+const { buildSchema } = require('graphql');
+const { graphqlKoa } = require('graphql-server-koa');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const app = new Koa();
-
-app.use(ctx => {
-  ctx.body = 'Hello World';
+const router = new KoaRouter();
+const PORT = 3000;
+const authors = [{ id: 1, firstName: 'Priscilla' }];
+const schema = `
+  type Author {
+    id: Int!
+    firstName: String
+  }
+  type Query {
+    authors: [Author]
+  }
+  schema {
+    query: Query
+  }
+`
+const resolvers = {
+  Query: {
+    authors() {
+      return authors;
+    }
+  }
+}
+const executableSchema = makeExecutableSchema({
+  typeDefs: schema,
+  resolvers: resolvers,
 });
 
-app.listen(3000);
+app.use(koaBody());
+
+router.post('/graphql', graphqlKoa({ schema }));
+router.get('/graphql', graphqlKoa({ schema }));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(PORT);
