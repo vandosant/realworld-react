be1480dd245c52d34f00801cb6ff6b38c54b907e
vandosant <sjskender@gmail.com>
2017-03-18 15:00:47 -0600

    graphql schema works now

--- server/src/app.js
+++ server/src/app.js
@@ -1,36 +1,24 @@
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
 const koaBody = require('koa-bodyparser');
-const { buildSchema } = require('graphql');
+const { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString } = require('graphql');
 const { graphqlKoa } = require('graphql-server-koa');
-const { makeExecutableSchema } = require('graphql-tools');
 
 const app = new Koa();
 const router = new KoaRouter();
 const PORT = 3000;
-const authors = [{ id: 1, firstName: 'Priscilla' }];
-const schema = `
-  type Author {
-    id: Int!
-    firstName: String
-  }
-  type Query {
-    authors: [Author]
-  }
-  schema {
-    query: Query
-  }
-`
-const resolvers = {
-  Query: {
-    authors() {
-      return authors;
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      hello: {
+        type: GraphQLString,
+        resolve() {
+          return 'world';
+        }
+      }
     }
-  }
-}
-const executableSchema = makeExecutableSchema({
-  typeDefs: schema,
-  resolvers: resolvers,
+  })
 });
 
 app.use(koaBody());
